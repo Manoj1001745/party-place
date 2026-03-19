c40fdcb51d8d125bf87c50c4b28d68c3270bf8cf
Manoj1001745 <khadkamanoj1001@gmail.com>
2026-03-19 18:52:08 +0545

    feat: configure image remote patterns in Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "party-place-public-assets.s3.amazonaws.com",
+      },
+    ],
+  },
+}
 
 export default nextConfig
